1d779cea8fb32622a9746135307b37814f5e51a1
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2022-08-01 15:07:45 -0400

    Signup page

--- shmeedron/shmeedron/settings.py
+++ shmeedron/shmeedron/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'leaderboard.apps.LeaderboardConfig',
+    'accounts',
 ]
 
 MIDDLEWARE = [
--- shmeedron/shmeedron/urls.py
+++ shmeedron/shmeedron/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('leaderboard/', include('leaderboard.urls')),
     path('', RedirectView.as_view(url='leaderboard/', permanent=True)),
+    path("accounts/", include("accounts.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
