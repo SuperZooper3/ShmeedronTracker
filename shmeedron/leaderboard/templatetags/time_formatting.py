7530857cad2fd86e004181a6886892122d0841c7
SuperZooper3 <54122151+SuperZooper3@users.noreply.github.com>
2022-08-02 17:07:11 -0400

    Way less jank timeclean function

--- shmeedron/leaderboard/templatetags/time_formatting.py
+++ shmeedron/leaderboard/templatetags/time_formatting.py
@@ -6,28 +6,13 @@ register = template.Library()
 def timeclean(value):
     """Strips the leading and trailing 0s from a time object and returns it's string. Arg is irrelevant"""
     assert(type(value)) == datetime.timedelta
-    time_string = str(value)
-    out_string = ""
-    write_buffer = ""
-    non_values = ["0",":"]
-    milisecond_flag = "."
-    writing = False
-    miliseconds = False
 
-    for v in time_string:
-        if v not in non_values:
-            writing = True
-        
-        if writing:
-            write_buffer += v
-            if v == milisecond_flag:
-                miliseconds = True
+    total = value.total_seconds()
+    hours = int(total // 3600)
+    minutes = int((total % 3600) // 60)
+    seconds = int(total % 60)
+    miliseconds = int((total % 1) * 1000)
 
-            if v not in non_values or not miliseconds:
-                out_string += write_buffer
-                write_buffer = ""
+    return '{:02}:{:02}:{:02}.{:03}'.format(hours, minutes, seconds, miliseconds).lstrip("0:")
 
-    return out_string
-
-# print(clean_time(datetime.timedelta(minutes=10, hours=0, seconds=0, milliseconds=15)))
 register.filter("timeclean",timeclean)
